108b42185a73d10cca18a028c9679138fb6dc2d8
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2023-01-05 15:02:24 +0300

    2. JCIP. Настройка библиотеки [#268575]

---
+++ src/main/java/ru/job4j/Count.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+/*
+ThreadSafe- говорит пользователям данного класса,
+что класс можно использовать в многопоточном режиме.
+ */
+@ThreadSafe
+public class Count {
+    /*
+    @GuardedBy("this")- выставляется над общим ресурсом.
+    Аннотация имеет входящий параметр.
+    Он указывает на монитор, по которому мы будем синхронизироваться.
+     */
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized void increment() {
+        this.value++;
+    }
+
+    public synchronized int get() {
+        return this.value;
+    }
+}
